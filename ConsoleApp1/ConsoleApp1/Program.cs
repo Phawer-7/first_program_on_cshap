019d9a01d64b1774b4b251ffcac5ec722ad34f07
Phawer <69140238+Phawer-7@users.noreply.github.com>
2020-09-12 11:13:36 +0500

    update
    новые знания :)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,7 +7,14 @@ using System.Threading.Tasks;
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, world\nNew Line");
+            Console.WriteLine("Hello, world\nHow are you?");
+            // user 
+            string lol = Console.ReadLine();
+            // string to short
+            short x = Convert.ToInt16(lol);
+            // Write
+            Console.Write("Okay!\n");
+            Console.WriteLine(x);
             Console.ReadKey();
         }
     }
