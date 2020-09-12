9ce81b58319f747e18fac69e632ee0c597d078b1
Phawer <69140238+Phawer-7@users.noreply.github.com>
2020-09-12 18:30:20 +0500

    Этот день останется в Истории

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "My Console App";
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Vzlom");
+            Console.ReadKey();
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,14 +7,36 @@ using System.Threading.Tasks;
 namespace ConsoleApp1 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, world\nHow are you?");
+            Console.WriteLine("Hello, world\nInput random number:");
             // user 
             string lol = Console.ReadLine();
             // string to short
             short x = Convert.ToInt16(lol);
             // Write
             Console.Write("Okay!\n");
-            Console.WriteLine(x);
+            
+            // if and else
+            if (x == 100)
+            {
+                Console.WriteLine("100");
+            }
+            else {
+                Console.WriteLine("XZ");
+            }
+
+            // switch
+            switch(x)
+            {
+                case 1:
+                    Console.WriteLine("lolol");
+                    break;
+                case 10:
+                    Console.WriteLine("Lol");
+                    break;
+                default:
+                    Console.WriteLine("XZ");
+                    break;
+            }
             Console.ReadKey();
         }
     }
